b5f7137f6f6a082f5d5444bbb8e05b5a1bed369e
4surecarlo <carlo@4sure.com.au>
2022-10-03 15:24:33 +0800

    added plugin updater

--- disable-client-avada-builder.php
+++ disable-client-avada-builder.php
@@ -9,6 +9,14 @@
  * Author: 4sure
  * Author URI: https://4sure.com.au
  */
+include_once( plugin_dir_path( __FILE__ ) . 'updater.php');
+$updater = new Disable_non_admin_avada_updater( __FILE__ ); // instantiate our class
+$updater->set_username( '4surecarlo' ); // set username
+$updater->set_repository( 'Avada-Disable-Builder-for-Non-Admins' ); // set repo
+$updater->initialize(); // initialize the updater
+if( ! class_exists( 'Disable_non_admin_avada_updater' ) ){
+	include_once( plugin_dir_path( __FILE__ ) . 'updater.php' );
+}
 function disable_client_avada_builder( $post_types ) {
     if(current_user_can('administrator')){
         $post_types[] = 'page';
